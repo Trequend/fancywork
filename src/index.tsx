0cb393ca9d61eaf26bb801e138e5b97aed2ad053
Trequend <68006090+Trequend@users.noreply.github.com>
2021-08-30 22:04:59 +0300

    feat(frontend): add global css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import './styles/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
