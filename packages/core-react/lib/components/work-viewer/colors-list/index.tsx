a88bc0455063a5533ba95fca23dd379d521491fd
Trequend <68006090+Trequend@users.noreply.github.com>
2021-10-31 22:38:37 +0300

    feat(core-react): improve color list borders

--- packages/core-react/lib/components/work-viewer/colors-list/index.tsx
+++ packages/core-react/lib/components/work-viewer/colors-list/index.tsx
@@ -1,6 +1,6 @@
 import { CheckOutlined } from '@ant-design/icons';
 import { WorkViewProvider, getContrastColor, Palette } from '@fancywork/core';
-import { FC, useEffect, useMemo, useRef, useState } from 'react';
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styles from './index.module.scss';
 
 type Props = {
@@ -33,7 +33,7 @@ export const ColorsList: FC<Props> = ({
     });
   }, [viewProvider, palette]);
 
-  useEffect(() => {
+  const updateBorders = useCallback(() => {
     if (divRef.current) {
       setLeftBorderVisible(divRef.current.scrollLeft !== 0);
       setRightBorderVisible(
@@ -43,25 +43,26 @@ export const ColorsList: FC<Props> = ({
     }
   }, []);
 
+  useEffect(() => {
+    updateBorders();
+
+    const listener = () => {
+      updateBorders();
+    };
+
+    window.addEventListener('resize', listener);
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, [updateBorders]);
+
   return (
     <div
       className={`${styles.container} ${className} ${
         leftBorderVisible ? styles.leftBorder : ''
       } ${rightBorderVisible ? styles.rightBorder : ''}`}
     >
-      <div
-        ref={divRef}
-        className={styles.content}
-        onScroll={() => {
-          if (divRef.current) {
-            setLeftBorderVisible(divRef.current.scrollLeft !== 0);
-            setRightBorderVisible(
-              divRef.current.scrollLeft + divRef.current.clientWidth <
-                divRef.current.scrollWidth
-            );
-          }
-        }}
-      >
+      <div ref={divRef} className={styles.content} onScroll={updateBorders}>
         {colors.map(({ code, hex, symbol, symbolColor }) => {
           const statistics = viewProvider.getColorStatistics(code);
           if (statistics.cellsCount === statistics.embroideredCellsCount) {
